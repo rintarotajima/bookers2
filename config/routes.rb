836348b3f48a593345641816302d82969ecda692
rintarotajima <rintarotajima@gmail.com>
2023-10-06 14:44:17 +0000

    [Add] headerレイアウトの作成(途中)

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
+  get 'home/about' => 'homes#about'
 end
