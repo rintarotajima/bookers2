97ce51cdf8ff15276ef0f8b47b7852a56aead757
rintarotajima <rintarotajima@gmail.com>
2023-10-06 08:55:54 +0000

    [Add] books_controllerにアクションを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
 class BooksController < ApplicationController
+  def index
+    
+  end
+  
+  def create
+    
+  end
+  
+  def show
+    
+  end
+    
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
 end
