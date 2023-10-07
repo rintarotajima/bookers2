cd91d99606127d38b741c67546583b567a6b2df8
rintarotajima <rintarotajima@gmail.com>
2023-10-07 09:43:57 +0000

    [Add] ログイン、ログアウト後の遷移先の指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  
+  def after_sign_in_path_for(resource)
+    home_about_path
+  end
   protected
   
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sing_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
   end
 end
