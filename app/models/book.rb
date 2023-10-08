826334319230b83fee243f4245c2be171bbc01dd
rintarotajima <rintarotajima@gmail.com>
2023-10-08 07:10:59 +0000

    [Update] bookのindexページの左側(New book)

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,5 @@ class Book < ApplicationRecord
   has_one_attached :profile_image
   belongs_to :user
   
-  def get_profile_image
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/default-image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: "image/jpeg")
-    end
-    image
-  end
+  
 end
