809138b53c98ac853ce7795cb95a10101f3c3bfe
rintarotajima <rintarotajima@gmail.com>
2023-10-09 06:30:40 +0000

    [Update] Create機能の実装やshowページの作成(途中)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,37 +2,43 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def index
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to books_path
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
-  
+
   def show
-    
+    @book = Book.find(params[:id])
+
   end
-    
+
   def edit
-    
+
   end
-  
+
   def update
-    
+
   end
-  
+
   def destroy
-    
+
   end
+
   
   private
   
   def book_params
-    params.require(:book).permit(:title, :body, :profile_image)
+    params.require(:book).permit(:title, :body)
   end
-  
+
 end
