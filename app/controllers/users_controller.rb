344e1dbda53a39921a6a0ceb753d0cd34ad41414
rintarotajima <rintarotajima@gmail.com>
2023-10-13 14:12:46 +0000

    [Add] userのshowページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   
-  def new 
-    @book = Book.new
-  end
-  
   def index
    @books = Book.all
    @book = Book.new
    @user = current_user
+  end
+  
+  def show
+    @user = current_user
+    @books = @user.books
   end
   
 end
