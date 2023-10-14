a20fe431a3a5f22b438e1ff47391512800f18e24
rintarotajima <rintarotajima@gmail.com>
2023-10-14 13:54:58 +0000

    [Add] userのupdate機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,31 @@
 class UsersController < ApplicationController
-  
+
   def index
    @books = Book.all
    @book = Book.new
    @user = current_user
   end
-  
+
   def show
     @user = current_user
     @books = @user.books
   end
-  
+
   def edit
-    @user = User.find(params[:id])      
+    @user = User.find(params[:id])
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+
 end
