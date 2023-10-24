c1fdd97821e105dd973a6e5414c44102d6cb4629
rintarotajima <rintarotajima@gmail.com>
2023-10-24 14:23:02 +0000

    [Update] Rspecテストの修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :index, :create, :show, :edit, :destroy, :update] do
-    resource :book_comments, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
